ff656b14c1eb7cd07e7e72fe26bca37f74bdbfd9
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2023-04-12 13:06:24 +0300

    gradle updates

--- app/src/main/java/com/atilsamancioglu/kotlinalertdialog/MainActivity.kt
+++ app/src/main/java/com/atilsamancioglu/kotlinalertdialog/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
